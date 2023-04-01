22803ab0e1c0ba3333c9cce352d8cfa7bd402e61
Антон Мурашкин <anton@DESKTOP-GG7J7RE.localdomain>
2023-04-01 09:31:03 +0300

    demo commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
+
+const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+
+program.parse();
+
